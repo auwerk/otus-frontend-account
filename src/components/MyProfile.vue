da60851264bfc37bf523177dc507621290ef672c
auwerk <noisefront@yandex.ru>
2023-03-09 08:27:39 +0300

    Simple MyProfile component implementation

--- src/components/MyProfile.vue
+++ src/components/MyProfile.vue
@@ -1,9 +1,51 @@
 <template>
-  <div class="my-profile"></div>
+  <div class="my-profile">
+    <table>
+      <tr>
+        <td>Имя пользователя</td>
+        <td>{{ myProfile.userName }}</td>
+      </tr>
+      <tr>
+        <td>Адрес электронной почты</td>
+        <td>{{ myProfile.email }}</td>
+      </tr>
+      <tr>
+        <td>Имя, Фамилия</td>
+        <td>{{ myProfile.firstName }} {{ myProfile.lastName }}</td>
+      </tr>
+      <tr>
+        <td>Дата рождения</td>
+        <td>{{ myProfile.birthDate }}</td>
+      </tr>
+      <tr>
+        <td>Номер телефона</td>
+        <td>{{ myProfile.phoneNumber }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'MyProfile',
+  data() {
+    return {
+      myProfile: {
+        userName: '',
+        email: '',
+        firstName: '',
+        lastName: '',
+        birthDate: '',
+        phoneNumber: '',
+      },
+    };
+  },
+  mounted() {
+    axios.get('/api/user/profile').then(function(response) {
+      this.myProfile = response.data;
+    }.bind(this));
+  },
 };
 </script>
