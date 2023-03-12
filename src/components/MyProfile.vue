872c8b3f83bccf442ff3bc8c5e4ff5f45b485fc9
auwerk <noisefront@yandex.ru>
2023-03-12 22:26:59 +0300

    Profile editing

--- src/components/MyProfile.vue
+++ src/components/MyProfile.vue
@@ -1,27 +1,65 @@
 <template>
-  <div class="my-profile">
+  <div class="my-profile-view" v-if="!editMode">
     <table>
       <tr>
-        <td>Имя пользователя</td>
+        <td class="prop-key">Имя пользователя</td>
         <td>{{ myProfile.userName }}</td>
       </tr>
       <tr>
-        <td>Адрес электронной почты</td>
+        <td class="prop-key">Адрес электронной почты</td>
         <td>{{ myProfile.email }}</td>
       </tr>
       <tr>
-        <td>Имя, Фамилия</td>
+        <td class="prop-key">Имя, Фамилия</td>
         <td>{{ myProfile.firstName }} {{ myProfile.lastName }}</td>
       </tr>
       <tr>
-        <td>Дата рождения</td>
+        <td class="prop-key">Дата рождения</td>
         <td>{{ myProfile.birthDate }}</td>
       </tr>
       <tr>
-        <td>Номер телефона</td>
+        <td class="prop-key">Номер телефона</td>
         <td>{{ myProfile.phoneNumber }}</td>
       </tr>
+      <tr>
+        <td colspan="2"><button v-on:click="editProfile">Редактировать</button></td>
+      </tr>
     </table>
+  </div>
+  <div class="my-profile-edit" v-if="editMode">
+    <form>
+      <table>
+        <tr>
+          <td class="prop-key">Имя пользователя</td>
+          <td><input v-model="myProfile.userName" readonly/></td>
+        </tr>
+        <tr>
+          <td class="prop-key">Адрес электронной почты</td>
+          <td><input v-model="myProfile.email" placeholder="vanya@mail.ru"/></td>
+        </tr>
+        <tr>
+          <td class="prop-key">Имя</td>
+          <td><input v-model="myProfile.firstName" placeholder="Иван"/></td>
+        </tr>
+        <tr>
+          <td class="prop-key">Фамилия</td>
+          <td><input v-model="myProfile.lastName" placeholder="Иванов"/></td>
+        </tr>
+        <tr>
+          <td class="prop-key">Дата рождения</td>
+          <td><input v-model="myProfile.birthDate" placeholder="01.01.1961"/></td>
+        </tr>
+        <tr>
+          <td class="prop-key">Номер телефона</td>
+          <td><input v-model="myProfile.phoneNumber" placeholder="89001617878"/></td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <button v-on:click="saveChanges">Сохранить</button>
+          </td>
+        </tr>
+      </table>
+    </form>
   </div>
 </template>
 
@@ -32,6 +70,7 @@ export default {
   name: 'MyProfile',
   data() {
     return {
+      editMode: false,
       myProfile: {
         userName: '',
         email: '',
@@ -43,9 +82,24 @@ export default {
     };
   },
   mounted() {
-    axios.get('/api/user/profile').then(function(response) {
-      this.myProfile = response.data;
-    }.bind(this));
+    axios.get('/api/user/profile')
+      .then(function(response) { this.myProfile = response.data; }.bind(this));
   },
+  methods: {
+    editProfile() {
+      this.editMode = true;
+    },
+    saveChanges() {
+      axios.put('/api/user/profile', this.myProfile)
+          .then(function(response) { console.log(response); this.editMode = false; }.bind(this))
+          .catch(function(response) { console.log(response); alert('Ошибка!') });
+    }
+  }
 };
 </script>
+
+<style>
+td.prop-key {
+  text-align: right;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,12 @@ module.exports = defineConfig({
   publicPath: '/account',
   devServer: {
     proxy: {
-      '/api/user': {
+      '/api/user/profile': {
         target: 'http://arch.homework',
         changeOrigin: true,
+        headers: {
+          'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJiSjNtRUJ3QUVzdVlHVm5jRjhoanREOERkMVUwaTlXWXotUmN0d3hzTjFVIn0.eyJleHAiOjE2Nzg2NDkyNDAsImlhdCI6MTY3ODY0ODk0MCwiYXV0aF90aW1lIjoxNjc4NjQ3MjU0LCJqdGkiOiIxMTI3MDBlOC0wMWVmLTRkNmUtYTYxZS0zZDQxZjE0MzUzZTAiLCJpc3MiOiJodHRwOi8va2V5Y2xvYWsuYXJjaC5ob21ld29yay9yZWFsbXMvb3R1cyIsImF1ZCI6Im90dXMtc2VydmljZS11c2VyIiwic3ViIjoiNTMxODVlNTQtMjg3Ny00MmE1LTgzYjMtNTE5NTMxOWZlNzliIiwidHlwIjoiSUQiLCJhenAiOiJvdHVzLXNlcnZpY2UtdXNlciIsIm5vbmNlIjoibDdKOGNUQUFYbjg5eTN1LVdnUl9hYUQ5MUNRQlQyT0xqWEphYkNsa3BobyIsInNlc3Npb25fc3RhdGUiOiIxMjExMDU5OS04NWNjLTRjMDgtYWUwZi00Y2M3YWI2Njc0MWMiLCJhdF9oYXNoIjoiZ2NsVXZQNkZNRDUxZGJNaUdDRERUUSIsImFjciI6IjAiLCJzaWQiOiIxMjExMDU5OS04NWNjLTRjMDgtYWUwZi00Y2M3YWI2Njc0MWMiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6IkVkZGllIFN0YXJsaW5nIiwiZ3JvdXBzIjpbXSwicHJlZmVycmVkX3VzZXJuYW1lIjoiZWRkaWUiLCJnaXZlbl9uYW1lIjoiRWRkaWUiLCJmYW1pbHlfbmFtZSI6IlN0YXJsaW5nIiwiZW1haWwiOiJlZGRpZUB5YS5ydSJ9.PwJsVKToHrZVGNH6a2cwG_v_MdYM3RFUnIM2vMs2qKdzKFQRC3konQI5F0ycg9DVnGLHnOGm2O37TRwAiRSyePOIt_h-Hr7DMjNaJonG3mmFaSDaDg7DShxRo3HEUby-PQAroqHrKkkStlw3DNMn9m1Fm2GVRsnGS7tsp44jFnjL5XUwlMPXO5nl40pR3pV0QxyG7Vo-52kz0M55RRPv0S0SmgT0auIhWlE9d4p2RQGQ7P_RnBZzdaoSyT1ALhdJVGyQDrczKdm4h9Vvl6YJ9gZJCuUORCoo1RZPvFxitL6MNo9mCYwFybY4f93o4ByRz1aqg9ERv00hjiLZNacIsQ',
+        }
       },
     },
   },
